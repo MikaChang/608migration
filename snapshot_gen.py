1bbccd8222f253226e4294f63601f541adf6f818
Louis <louis.nw@hotmail.com>
2013-06-01 00:36:15 +0800

    add snapshot_gen

--- snapshot_gen.py
+++ snapshot_gen.py
@@ -6,4 +6,9 @@ def snapshot_gen(tot_host_num, src_num, migr_type):
     ### src_num can be [4, 8, 12] in 16_mode
     ### migr_type can be 'Consolidation' or 'LoadBalancing'
     
-    
+    if migr_type == 'Consolidation':
+        
+    elif migr_type == 'LoadBalancing':
+        
+    else 
+        assert(0)
